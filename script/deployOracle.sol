2fca6b2c1efd36db5641377cd91d60aa78c527ec
Komron Khisomov <normok_9595@bk.ru>
2024-07-10 18:48:39 +0500

    oracle

--- script/deployOracle.sol
+++ script/deployOracle.sol
@@ -5,12 +5,11 @@ import {Script, console} from "forge-std/Script.sol";
 import {Oracle} from "../src/Oracle.sol";
 
 contract OracleScript is Script {
-    Oracle oracle;
 
     function run() public returns (Oracle){
+        
         vm.startBroadcast();
         Oracle oracle = new Oracle();
-
         vm.stopBroadcast();
         return oracle;
 
--- src/Oracle.sol
+++ src/Oracle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
 contract Oracle {
 
 }
