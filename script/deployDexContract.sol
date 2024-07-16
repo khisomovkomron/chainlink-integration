c3318d49915a325cd9d1499feb03dc76ada20ef5
Komron Khisomov <normok_9595@bk.ru>
2024-07-16 19:35:09 +0500

    scripts and setup test

---
+++ script/deployDexContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DexContract} from "../src/DexContract.sol";
+
+contract DexContractScript is Script {
+
+    function run() public returns (DexContract){
+        
+        vm.startBroadcast();
+        DexContract dex = new DexContract(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        vm.stopBroadcast();
+        return dex;
+
+    }
+}
---
+++ test/dexCotract.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DexContractScript} from "../script/deployDexContract.sol";
+import {DexContract} from "../src/DexContract.sol";
+
+contract CounterTest is Test {
+    DexContract public dex;
+
+    function setUp() public {
+        DexContractScript dexScript = new DexContractScript();
+        dex = dexScript.run();
+    }
+
+}
